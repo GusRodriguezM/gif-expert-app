2c73c6c82bc2c50cb5fc1810f5a6b18d0b09b773
Gustavo RM <gustavo.rm.isc@gmail.com>
2021-05-26 22:03:24 -0500

    Tests in GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -7,14 +7,36 @@ describe('Tests in <GrifGridItem/>', () => {
    
     const title = 'Justa generic title';
     const url = 'https://localhost:3000/someImage.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
 
     
     test('Should render <GrifGridItem/> correctly', () => {
-
-        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+        
         expect(wrapper).toMatchSnapshot();
 
     });
+
+    test('should have a paragraph with the title', () => {
+       
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+
+    });
+    
+    test('the value of src and alt prop should be equal to the passed props', () => {
+        
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+
+    });
+
+    test('should have css class animate__fadeInLeftBig', () => {
+        
+        const animationClass = 'animate__fadeInLeftBig';
+        const div = wrapper.find('div');
+        expect(div.prop('className').includes(animationClass)).toBe(true);
+        
+    });
     
 });
-
