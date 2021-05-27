6f0864e9e12e1ccd215004d58361618587f5d249
Gustavo RM <gustavo.rm.isc@gmail.com>
2021-05-26 23:20:53 -0500

    Simulate changes in an input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,12 +5,24 @@ import AddCategory from "../../components/AddCategory";
 describe('Tests in <AddCategory/>', () => {
 
     const setCategories = () => {};
+    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
    
     test('Should render <AddCategory/> correctly', () => {
        
-        const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
         expect(wrapper).toMatchSnapshot();
         
     });
+
+    test('should simulate the change in the input text', () => {
+        
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+        
+        input.simulate('change', {target: {value}});
+
+        expect(wrapper.find('p').text().trim()).toBe(value);
+
+    });
+    
     
 });
