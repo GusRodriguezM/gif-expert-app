e685c7e18b42118e062a4f1fa7095c8d8cce1713
Gustavo RM <gustavo.rm.isc@gmail.com>
2021-05-27 04:05:40 -0500

    Evaluate the change of the state with waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,13 +3,26 @@ import {renderHook} from '@testing-library/react-hooks';
 
 describe('Tests in the useFetchGifs custom hook', () => {
    
-    test('should return the initial state', () => {
+    test('should return the initial state', async() => {
 
-        const {result} = renderHook(() => useFetchGifs('One Punch Man'));
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('One Punch Man'));
         const {data, loading} = result.current;
 
+        await waitForNextUpdate();
         expect(data.length).toBe(0);
         expect(loading).toBeTruthy();
+        
+    });
+    
+    test('should return an array of images and set loading to false', async () => {
+       
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('One Punch Man'));
+        await waitForNextUpdate();
+
+        const {data, loading} = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBeFalsy();
         
     });
     
